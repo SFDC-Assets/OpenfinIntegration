28612b6f0294ace11ea87dbcecb0cc9745934580
John M <john.mandia@gmail.com>
2021-09-21 16:51:09 +0100

    Add walkme integration example

---
+++ preload-walkme.js
@@ -0,0 +1,15 @@
+// Do not inject into iFrames
+if (window !== window.top) {
+  return;
+}
+const walkmeUrl =
+  "https://cdn.walkme.com/users/bb2caab25e424d66a5e69e08aff6e93c/test/walkme_bb2caab25e424d66a5e69e08aff6e93c_https.js";
+
+window.addEventListener("DOMContentLoaded", function () {
+  var walkme = document.createElement("script");
+  walkme.type = "text/javascript";
+  walkme.async = true;
+  walkme.src = walkmeUrl;
+  document.head.prepend(walkme);
+  window._walkmeConfig = { smartLoad: true };
+});
